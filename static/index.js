58da3aa7a2b86e9888055f4fd3bab76443b43b4b
leonardo185 <leonardo18598@gmail.com>
2018-12-22 10:49:51 +0530

    configured buttons

--- static/index.js
+++ static/index.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
   //Connect to the websocket.
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  //When connected configure buttons.
+  socket.on('connect', ()=>{
+
+    //Each button should should emit a "submit vote" event.
+    document.querySelectorAll('button').forEach(button=>{
+      button.onclick = ()=>{
+        const selection = button.dataset.vote;
+        socket.emit('submit vote',{'selection':selection});
+      }
+    });
+  })
+
+
+
+
 
 });
